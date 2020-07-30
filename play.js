2f80b721a93556fd3e51b9cd4f90c17ae7efd18a
TimaJuma <tima.xpl@gmail.com>
2020-07-30 00:37:20 +0000

    added seperate file to handle  input and exported

--- play.js
+++ play.js
@@ -1,34 +1,11 @@
 const {connect } = require('./client');
-const { stdin } = require('process');
+const {setupInput} = require('./input');
 
 
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    console.log('user quited the GAME with "ctr+c"');
-    process.exit();
-  }
-};
 
-// SET UP USER INTERFACE
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', (key)=> {
-    handleUserInput(key);
-  })
-
-  return stdin;
-}
-
-
-setupInput();
-
-//
 
 
 console.log('connecting from play.js...');
 connect();
 
+setupInput();
